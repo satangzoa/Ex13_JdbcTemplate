738c83cb58b13c7ee66cf5a7f473d5b1596c06e9
A <A@DESKTOP-NKSGQVR>
2023-05-10 09:38:33 +0900

    springboot

---
+++ src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,39 @@
+package com.study.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.study.springboot.jdbc.MyUserDAO;
+
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private MyUserDAO userDao;
+
+  
+    
+    @RequestMapping("/")
+    public @ResponseBody String root() throws Exception{
+        return "JdbcTEmplate 사용하기";
+    }
+
+    //@GetMapping("/userlist")
+    @RequestMapping(value = "/userlist", method = RequestMethod.GET)
+    public String userlistPage(Model model) {
+        model.addAttribute("users", userDao.list());
+        return "userlist";
+    }
+    
+    
+//    @RequestMapping("/userlist")
+//    public String home(){
+//        return "userlist";
+//    }
+}
